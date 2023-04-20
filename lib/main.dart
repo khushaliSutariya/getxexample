cc30551e851c0057a31b5a3d9c9f2732d88af606
khushalisutariya <124670882+khushalisutariya@users.noreply.github.com>
2023-04-20 18:40:08 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getxexample/screens/Homepage.dart';
+import 'package:getxexample/screens/MyStateExample.dart';
+
+void main() {
+  runApp(GetMaterialApp(
+    home: MyStateExample(),
+  ));
+}
